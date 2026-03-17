45e0c9b4907573f0d56d1fdaf7e04002de5c8dd3
Zac Skalko <zskallo@gmail.com>
2026-03-16 21:30:10 -0500

    Relax Cypress shell readiness assertion
    Wait for the terminal element itself instead of the UI status badge so the end-to-end test validates actual shell I/O without depending on transient connection copy.

--- cypress/e2e/shells.cy.js
+++ cypress/e2e/shells.cy.js
@@ -16,7 +16,7 @@ describe("shell sessions", () => {
     cy.visit("/");
 
     cy.contains("SHELL 1", { timeout: 30000 }).should("be.visible");
-    cy.contains("ONLINE", { timeout: 30000 }).should("be.visible");
+    cy.get(terminalTextarea, { timeout: 30000 }).should("exist");
 
     typeInTerminal("export LCARS_MARKER=one && printf 'ONE:%s\\n' \"$LCARS_MARKER\"");
     cy.get(terminalRows).should("contain.text", "ONE:one");
@@ -24,13 +24,11 @@ describe("shell sessions", () => {
     cy.contains("+ SHELL").click();
     cy.contains("SHELL 2", { timeout: 10000 }).should("be.visible");
     cy.contains("SHELL 2").click();
-    cy.contains("ONLINE", { timeout: 30000 }).should("be.visible");
 
     typeInTerminal("printf 'TWO:%s\\n' \"${LCARS_MARKER:-missing}\"");
     cy.get(terminalRows).should("contain.text", "TWO:missing");
 
     cy.contains("SHELL 1").click();
-    cy.contains("ONLINE", { timeout: 30000 }).should("be.visible");
 
     typeInTerminal("printf 'BACK:%s\\n' \"$LCARS_MARKER\"");
     cy.get(terminalRows).should("contain.text", "BACK:one");
@@ -39,7 +37,6 @@ describe("shell sessions", () => {
 
     cy.contains("SHELL 1", { timeout: 30000 }).should("be.visible");
     cy.contains("SHELL 2", { timeout: 30000 }).should("be.visible");
-    cy.contains("ONLINE", { timeout: 30000 }).should("be.visible");
 
     typeInTerminal("printf 'RELOAD_OK\\n'");
     cy.get(terminalRows).should("contain.text", "RELOAD_OK");
